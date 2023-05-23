17c6d383498cef4db813ebde48661af12dd18d97
Daniel_De_Oliveira <daniel.deoliveira@correo.unimet.edu.ve>
2023-05-23 02:02:34 -0400

    23/05

---
+++ src/app/App.java
@@ -0,0 +1,205 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+/*
+    1. Mejorar error de /File/verifyLines para evitar errores con archivos sin una estructura
+ */
+import gui.GraphsGUI;
+import gui.IslandsGUI;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import linkedList.LinkedList;
+import linkedList.Node;
+import user.Relations;
+import user.User;
+import utils.Utils;
+
+/**
+ *
+ * @author Daniel
+ */
+public class App {
+
+    public LinkedList<String> lines = new LinkedList();
+    private LinkedList<User> usersList = new LinkedList();
+    private LinkedList<Relations> relationsList = new LinkedList();
+
+    // <editor-fold defaultstate="collapsed" desc="GUI's"> 
+    public void showIslands() {
+        IslandsGUI islands = new IslandsGUI();
+        islands.setVisible(true);
+    }
+
+    public void showGraphs() {
+        GraphsGUI graphs = new GraphsGUI();
+        graphs.setVisible(true);
+    }
+    // </editor-fold> 
+
+    // <editor-fold defaultstate="collapsed" desc="File">
+    public void fileReader(boolean aux, JFrame MainFrame) {
+        // 1. New graph
+        if (aux) {
+            // 1.1. Change old to new window of JFileChooser
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+            }
+
+            // 1.2. JFileChoouse
+            JFileChooser fc = new JFileChooser();
+            fc.setDialogTitle("Seleccionar un archivo de texto (TXT)");
+            fc.setAcceptAllFileFilterUsed(false);
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("TXT", "txt");
+            fc.addChoosableFileFilter(filter);
+            int select = fc.showOpenDialog(MainFrame);
+
+            // 1.3. Success select
+            if (select == JFileChooser.APPROVE_OPTION) {
+                String originFile = fc.getSelectedFile().getAbsolutePath();
+                String nameFile = "graph.txt";
+                String destinationFile = System.getProperty("user.dir") + File.separator + "src\\App" + File.separator + nameFile;; // Ruta del archivo de destino
+                System.out.println(destinationFile);
+                try {
+                    // 1.3.1 Open file mode reader
+                    FileReader fileReader = new FileReader(originFile);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                    // 1.3.2 Open file mode writer
+                    FileWriter fileWriter = new FileWriter(destinationFile);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+                    String linea;
+
+                    // 1.3.3 Read origin file linea each line & write in the destination file
+                    while ((linea = bufferedReader.readLine()) != null) {
+                        bufferedWriter.write(linea);
+                        bufferedWriter.newLine();
+                    }
+
+                    // 1.3.4 Close files
+                    bufferedReader.close();
+                    bufferedWriter.close();
+
+                    // 1.3.5 Read file
+                    this.fileReader(false, null);
+
+                } catch (IOException e) {
+                    Utils.error("Error al leer el archivo!", false);
+                }
+            }
+
+        } // 2. Read graph
+        else {
+            // 1.1. Name file
+            String nameFile = "graph.txt";
+
+            // 1.2. Absolute path
+            String routeFile = System.getProperty("user.dir") + File.separator + "src\\App" + File.separator + nameFile;
+
+            // 1.3. Read file
+            File file = new File(routeFile);
+
+            // 1.4. Each lines of file
+            try {
+                // 1.4.1. Invoke scanner of lines
+                Scanner scanner = new Scanner(file);
+
+                // 1.4.2. ERROR: File empty
+                if (!scanner.hasNextLine()) {
+                    Utils.error("El archivo está vacío!", false);
+                } // 1.4.2. Read file
+                else {
+                    while (scanner.hasNextLine()) {
+                        // 1.4.3. Read line and save in linkedList
+                        String line = scanner.nextLine();
+                        this.lines.addLast(line);
+                    }
+                }
+
+                // 1.4.-1. Close scanner
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                Utils.error("No se encontró el archivo", false);
+            }
+        }
+
+    }
+
+    public void verifyLines() {
+        // 1. Users
+        {
+            Node aux = this.lines.getHead();
+
+            while (aux != null) {
+                String value = (String) aux.getData().toString().toLowerCase().replace(" ", "").replace("@", "");
+
+                if ("relaciones".equals(value)) {
+                    break;
+                }
+                if (!"usuarios".equals(value)) {
+                    String[] userSplit = value.split(",");
+                    User userAdding = new User(Integer.parseInt(userSplit[0]), userSplit[1]);
+                    usersList.addLast(userAdding);
+                }
+
+                aux = aux.getNextNode();
+            }
+        }
+
+        // 2. Relations
+        {
+            Node aux = this.lines.getHead();
+
+            while (aux != null) {
+                String value = (String) aux.getData().toString().toLowerCase();
+
+                if ("relaciones".equals(value)) {
+                    aux = aux.getNextNode();
+                    break;
+                }
+
+                aux = aux.getNextNode();
+            }
+
+            while (aux != null) {
+                String value = (String) aux.getData().toString().toLowerCase().replace(" ", "");
+                String[] relationsSplit = value.split(",");
+                Relations relationsAdding = new Relations(Integer.parseInt(relationsSplit[0]), Integer.parseInt(relationsSplit[1]), Integer.parseInt(relationsSplit[2]));
+                this.relationsList.addLast(relationsAdding);
+
+                aux = aux.getNextNode();
+            }
+        }
+    }
+
+    public void updateChanges() {
+
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Getters">
+    public LinkedList<User> getUserList() {
+        return this.usersList;
+    }
+
+    public LinkedList<Relations> getRelationsList() {
+        return this.relationsList;
+    }
+    // </editor-fold>
+}
---
+++ src/linkedList/LinkedList.java
@@ -0,0 +1,370 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedList;
+
+import user.Relations;
+import user.User;
+
+/**
+ *
+ * @author Daniel
+ */
+public class LinkedList<T> {
+    private Node<T> firstNode;
+    private Node<T> lastNode;
+    private int size;
+
+    // Ready: 2
+    // <editor-fold defaultstate="collapsed" desc="Constructor">
+    /**
+     * Constructor for create list without initial data
+     *
+     */
+    public LinkedList() {
+        this.lastNode = null;
+        this.firstNode = null;
+        this.size = 0;
+    }
+
+    /**
+     * Constructor for create list with initial data
+     *
+     * @param value Set value. Is the initial value
+     */
+    public LinkedList(T value) {
+        Node<T> n = new Node(value);
+        this.firstNode = n;
+        this.lastNode = n;
+        this.size++;
+    }
+    //</editor-fold>
+
+    // Ready: 1
+    // <editor-fold defaultstate="collapsed" desc="Getters">
+    /**
+     * Returns the number of elements in the list.
+     *
+     * @return The number of elements in the list.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    public Node<T> getHead() {
+        return this.firstNode;
+    }
+    //</editor-fold>
+
+    // Ready: 3
+    // <editor-fold defaultstate="collapsed" desc="Additives">
+    /**
+     * Add value in the last position
+     *
+     * @param value Value to add
+     */
+    public void addLast(T value) {
+        Node<T> newNode = new Node(value);
+
+        this.size++;
+        if (isEmpty()) {
+            this.firstNode = this.lastNode = newNode;
+        } else {
+            this.lastNode.setNext(newNode);
+            this.lastNode = newNode;
+        }
+    }
+
+    /**
+     * Add value in the first position
+     *
+     * @param value Value to add
+     */
+    public void addFirst(T value) {
+        Node<T> newNode = new Node(value);
+
+        this.size++;
+        if (isEmpty()) {
+            this.firstNode = this.lastNode = newNode;
+        } else {
+            newNode.setNext(this.firstNode);
+            this.firstNode = newNode;
+        }
+    }
+
+    /**
+     * Add values in a determinate position
+     *
+     * @param value Value of data
+     * @param position Position in list
+     */
+    public void add(T value, int position) {
+        // Invalid position: -1, -100, -44
+        if (position < 0) {
+            throw new IndexOutOfBoundsException("Position is lower than 0");
+        } // Invalid position (Size = 10): 11, 15, 20
+        else if (position > this.size - 1) {
+            throw new IndexOutOfBoundsException("Position is higher that size list");
+        } // If list is empty
+        else if (this.isEmpty() || position == 0) {
+            this.addFirst(value);
+        } // If is the last position
+        else if (position == this.size - 1) {
+            this.addLast(value);
+        } // Search position
+        else {
+            Node<T> n = new Node(value);
+
+            // 1. Firts, take a head of the list to for to the specific position. Second,
+            // count the position
+            Node<T> aux = this.firstNode;
+            int count = 0;
+
+            // 2. For all the list to go to the specific position
+            while (count < position - 1) {
+                aux = aux.getNextNode();
+                count++;
+            }
+
+            // 3. Update chain
+            n.setNext(aux.getNextNode());
+            aux.setNext(n);
+        }
+    }
+    //</editor-fold>
+
+    // Ready: 3
+    // <editor-fold defaultstate="collapsed" desc="Removers">
+    /**
+     * Delete last value (node) of list (chain)
+     */
+    public void deleteLast() {
+        if (this.isEmpty()); else {
+            LinkedList<T> newList = new LinkedList<>();
+            Node aux = this.firstNode;
+
+            for (int i = 0; i < this.size - 1; i++) {
+                newList.addLast((T) aux.getData());
+                aux = aux.getNextNode();
+            }
+
+            this.firstNode = newList.firstNode;
+            this.lastNode = newList.lastNode;
+            this.size--;
+        }
+    }
+
+    /**
+     * Delete first value (node) of list (chain)
+     */
+    public void deleteFirst() {
+        if (this.isEmpty()); else {
+            this.firstNode = this.firstNode.getNextNode();
+            this.size--;
+        }
+    }
+
+    /**
+     * Function to eliminate a data (node reality) of list (chain)
+     *
+     * @param position Position to eliminate
+     */
+    public void delete(int position) {
+        if (this.isEmpty())
+            ; else {
+            // Position < 0: Error
+            if (position < 0) {
+                throw new IndexOutOfBoundsException("Position is lower than 0");
+            } // Position > size - 1: Error
+            else if (position > this.size - 1) {
+                throw new IndexOutOfBoundsException("Position is higher that size list");
+            } // Position = 0: Firts
+            else if (position == 0) {
+                this.deleteFirst();
+            } // Position = Size - 1: Last
+            else if (position == this.size - 1) {
+                this.deleteLast();
+            } // Position ex: 7
+            else {
+                Node<T> aux = this.firstNode;
+
+                for (int i = 0; i < position - 1; i++) {
+                    aux = aux.getNextNode();
+                }
+
+                aux.setNext(aux.getNextNode().getNextNode());
+                this.size--;
+            }
+        }
+    }
+    //</editor-fold>
+
+    // Ready: 4
+    // <editor-fold defaultstate="collapsed" desc="Features">
+    /**
+     * Returns whether the list is empty or not.
+     *
+     * @return True if the list is empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return (this.firstNode == null);
+    }
+
+    /**
+     * Returns the element at a specific position in the list.
+     *
+     * @param position The position of the element to return.
+     * @return The element at the specified position, or null if the position is
+     * invalid.
+     */
+    public T get(int position) {
+        if (position < 0 || position >= this.size) {
+            throw new IndexOutOfBoundsException("Position invalid: '" + position + "'");
+        }
+
+        Node<T> currentNode = this.firstNode;
+        for (int i = 0; i < position; i++) {
+            currentNode = currentNode.getNextNode();
+        }
+
+        return currentNode.getData();
+    }
+
+    /**
+     * Verify if back to front is equals front to back
+     *
+     * @return Return true/false if class is palindrome
+     */
+    public boolean getPalindrome() {
+        LinkedList<T> compareer = new LinkedList<>();
+        Node<T> aux = this.firstNode;
+
+        // Algorithm
+        while (aux != null) {
+            compareer.addFirst(aux.getData());
+            aux = aux.getNextNode();
+        }
+        compareer.lastNode.setNext(null);
+
+        // Algorithm
+        boolean returned = true;
+        aux = this.firstNode;
+        Node<T> aux2 = compareer.firstNode;
+        while (aux != null) {
+            if (aux.getData() != aux2.getData()) {
+                returned = false;
+                break;
+            }
+
+            aux2 = aux2.getNextNode();
+            aux = aux.getNextNode();
+        }
+
+        return returned;
+    }
+
+    /**
+     * Order list to lower at higher numbers. Finish with string type
+     */
+    public void order() {
+        // List empty
+        if (this.isEmpty()); // List with 1 item
+        else if (this.size == 1); // List with +1 item
+        else {
+            // String
+            if (this.firstNode.getData().getClass() == String.class) {
+
+            } // Integer
+            else if (this.firstNode.getData().getClass() == Integer.class) {
+                Node<T> aux = this.firstNode;
+
+                for (int i = 0; i < this.size; i++) {
+                    Node<T> current = aux;
+
+                    while (current.getNextNode() != null) {
+                        int x = (int) current.getData();
+                        T xTemp = current.getData();
+                        int y = (int) current.getNextNode().getData();
+                        T yTemp = current.getNextNode().getData();
+
+                        if (x > y) {
+                            current.getNextNode().setData(xTemp);
+                            current.setData(yTemp);
+                        }
+
+                        current = current.getNextNode();
+                    }
+                }
+
+            } // Double
+            else if (this.firstNode.getData().getClass() == Double.class) {
+                Node<T> aux = this.firstNode;
+
+                for (int i = 0; i < this.size; i++) {
+                    Node<T> current = aux;
+
+                    while (current.getNextNode() != null) {
+                        double x = (double) current.getData();
+                        T xTemp = current.getData();
+                        double y = (double) current.getNextNode().getData();
+                        T yTemp = current.getNextNode().getData();
+
+                        if (x > y) {
+                            current.getNextNode().setData(xTemp);
+                            current.setData(yTemp);
+                        }
+
+                        current = current.getNextNode();
+                    }
+                }
+
+            } // Error
+            else {
+                throw new ClassCastException("Only work with string, integer and double");
+            }
+        }
+    }
+    //</editor-fold>
+
+    // Ready: 3
+    // <editor-fold defaultstate="collapsed" desc="Helpers">
+    /**
+     * This is for view the data in list IN CONSOLE
+     *
+     */
+    public void helperShow() {
+        Node<T> aux = this.firstNode;
+
+        while (aux != null) {
+            T value = aux.getData();
+            System.out.println(value);
+
+            aux = aux.getNextNode();
+        }
+    }
+
+    public void helperShowUser() {
+        Node<T> aux = this.firstNode;
+
+        while (aux != null) {
+            User values = (User) aux.getData();
+            System.out.println("ID: " + values.getId() + " || Username: " + values.getUser());
+
+            aux = aux.getNextNode();
+        }
+    }
+
+    public void helperShowRelations() {
+        Node<T> aux = this.firstNode;
+
+        while (aux != null) {
+            Relations values = (Relations) aux.getData();
+            System.out.println("User 1: " + values.getUser1() + " || User 2: " + values.getUser2() + " || Time: " + values.getTime());
+
+            aux = aux.getNextNode();
+        }
+    }
+    //</editor-fold>
+}
